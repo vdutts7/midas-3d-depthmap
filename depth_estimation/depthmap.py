4ddefe4c374b6a4ff30c2ad320c91be6d5e88cd2
vd7 <me@vd7.io>
2022-10-17 04:01:00 +0100

    DepthMap transform setup

--- depth_estimation/depthmap.py
+++ depth_estimation/depthmap.py
@@ -7,3 +7,16 @@ class DepthMapper:
         self.setup_model(accuracy)
         self.setup_device()
         self.setup_transforms(accuracy)
+
+    def setup_device(self):
+        # Set computation device based on CUDA availability
+        compute_device = "cuda" if torch.cuda.is_available() else "cpu"
+
+
+        self.depth_model.eval()
+        print(f'Depth model ready, using {self.device}')
+    
+    def setup_transforms(self, accuracy):
+        # Load and set image transformations based on model type
+        transforms = torch.hub.load("intel-isl/MiDaS", "transforms")
+    
